2fbff1b85de1a2af51b456d6e0bbfa0958b7ec8b
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-06-12 21:19:39 -0700

    added some regex to remove non-word characters except hyphens, URLs, and roman numeral strings

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# merrer
+# mapred 
 M(app)er (and) R(educ)er
 
 [G4G](https://www.geeksforgeeks.org/hadoop-streaming-using-python-word-count-problem/) with comments removed, utf tag added, and converted to Python3. 
 
 Works over text files with a Python3 installed and execute permissions.
 
+Removes URL strings and roman numerals.  Keeps most hyphenated words.
+
 ```
 mapred streaming \
-  -input ??? \
-  -output ??? \
+  -input <path to text files> \
+  -output <path to output directory> \
   -mapper mapper.py \
   -reducer reducer.py \
   -file mapper.py \
--- mapper.py
+++ mapper.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys 
-  
-for line in sys.stdin: 
-    line = line.strip() 
-    words = line.split() 
+import sys
+import re
+
+urls = re.compile('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}')
+romanNumerals = re.compile('^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$')
+
+for line in sys.stdin:
+    line = re.sub(r'[\d_]', '', line)
+    if re.search(urls, line):
+        line = re.sub(urls, '', line)
+    line = line.strip()
+    words = line.split()
     for word in words:
-        print('%s\t%s' % (word, 1))
+        word = re.sub('\-\-+', '', word)
+        word = re.sub(r'[^\w-]', '', word)
+        if re.search(romanNumerals, word) and word != "I":
+            word = re.sub(romanNumerals, '', word)
+        word = word.lower()
+        if (word != '') and (len(word) > 1 or word == 'a' or word == 'i'):
+            print('%s\t%s' % (word, 1))
